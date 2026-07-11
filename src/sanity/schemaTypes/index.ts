912ad4a9787a73955061e580d17338bd14982dc9
sarajittbiswas <sb.sarojit@gmail.com>
2026-07-12 00:26:45 +0530

    Purge Sanity CMS to resolve React 19 useEffectEvent build crash

--- src/sanity/schemaTypes/index.ts
+++
@@ -1,7 +0,0 @@
-import { type SchemaTypeDefinition } from 'sanity'
-import { author } from './author'
-import { article } from './article'
-
-export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [author, article],
-}
